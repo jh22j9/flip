acaeeb3a90580b82e6c5a2f135772e175a984d2d
jheeju <jheeju.dev@gmail.com>
2021-12-09 17:39:44 +0900

    feat(header): 헤더 컴포넌트 구현

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {useNavigate} from 'react-router'
+
+function Header() {
+
+  const navigate = useNavigate();
+  
+  const onCreate = () => {
+    navigate('/create')
+  }
+
+  return (
+    <header>
+      <div className="header-top"></div>
+      <div className="header-main">
+        <div className="header-main__content">
+          <a href="/">🃏 Fl!p</a>
+          <div>
+            <button className="create-btn" onClick={onCreate}>➕ Create a board</button>
+            <button className="signIn-btn">Sign in</button>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "../components/Header";
 import Boards from "../components/Boards";
 
 function Main() {
   return (
     <>
+      <Header />
       <Boards />
     </>
   );
