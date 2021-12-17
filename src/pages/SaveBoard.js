ef060c70a2323c55ceb4678b37ca5dfbd72cec3e
jheeju <jheeju.dev@gmail.com>
2021-12-17 16:44:58 +0900

    feat(save-board): 폰트 크기 변경 및 onKeyDown 이벤트 적용

--- src/pages/SaveBoard.js
+++ src/pages/SaveBoard.js
@@ -5,15 +5,32 @@ import Cards from "../components/Cards";
 function SaveBoard() {
   const cards = Array(9).fill(0); // TODO: state 불러오기
 
+  const onEnter = (e) => {
+    if (e.key.toLowerCase() === "enter") {
+      const form = e.target.form;
+      const index = [...form].indexOf(e.target);
+      form.elements[index + 1].focus();
+      e.preventDefault();
+    }
+  };
+
   const img = <img></img>;
   const content = (
     <div className="content-wrapper">
       <Cards cards={cards} content={img} />
-      <div className="input-fields">
-        <input type="text" placeholder="보드 제목을 입력하세요."></input>
-        <input type="text" placeholder="설명을 덧붙여보세요."></input>
+      <form className="input-fields">
+        <input
+          type="text"
+          placeholder="보드 제목을 입력하세요."
+          onKeyDown={onEnter}
+        ></input>
+        <input
+          type="text"
+          placeholder="설명을 덧붙여보세요."
+          onKeyDown={onEnter}
+        ></input>
         <input type="text" placeholder="태그를 추가해보세요."></input>
-      </div>
+      </form>
     </div>
   );
   return <MakeBoard content={content} nextRoute="play" />;
